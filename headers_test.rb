c2950eca1f90f47dcc5db7e134786334bebb68e3
Chris LaRose <chris.larose@mycase.com>
2015-06-12 11:08:40 -0700

    cjl - Add test for net http

--- headers_test.rb
+++ headers_test.rb
@@ -15,4 +15,15 @@ class HeadersTest < MiniTest::Unit::TestCase
     response = make_request_httparty
     assert_equal 'Hello World', response.body
   end
+
+  def test_request_net_http
+    stub_request(:get, "https://api.github.com/users/octocat/orgs")
+      .with(:headers => { 'Accept' => '*/*',
+                          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                          'User-Agent' => 'Ruby',
+                          'Authorization' => 'Bearer 1234' })
+      .to_return(:status => 200, :body => "Hello World", :headers => {})
+    response = make_request_net_http
+    assert_equal 'Hello World', response.body
+  end
 end
