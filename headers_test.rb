4beda700528ae543b70c9ba8246fec940ab5944e
Chris LaRose <chris.larose@mycase.com>
2015-06-12 11:18:36 -0700

    cjl - Remove http party test

--- headers_test.rb
+++ headers_test.rb
@@ -8,14 +8,6 @@ class HeadersTest < MiniTest::Unit::TestCase
     WebMock.disable_net_connect!
   end
 
-  def test_request_httparty
-    stub_request(:get, "https://api.github.com/users/octocat/orgs")
-      .with(:headers => {'Authorization'=>'Bearer 1234'})
-      .to_return(:status => 200, :body => "Hello World", :headers => {})
-    response = make_request_httparty
-    assert_equal 'Hello World', response.body
-  end
-
   def test_request_net_http
     stub_request(:get, "https://api.github.com/users/octocat/orgs")
       .with(:headers => { 'Accept' => '*/*',
